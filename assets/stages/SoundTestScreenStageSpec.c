8a4838582f01a9d398aeed05c0a567e13b65ab91
Christian Radke <chris@vr32.de>
2019-08-14 18:47:26 +0200

    Catching up with IDE changes

--- assets/stages/SoundTestScreenStageSpec.c
+++ assets/stages/SoundTestScreenStageSpec.c
@@ -26,6 +26,7 @@
 #include <Stage.h>
 #include <Printing.h>
 #include <CharSet.h>
+#include <Fonts.h>
 
 
 //---------------------------------------------------------------------------------------------------------
@@ -33,7 +34,6 @@
 //---------------------------------------------------------------------------------------------------------
 
 extern EntitySpec LOW_POWER_INDICATOR_LB;
-extern FontSpec VUENGINE_FONT;
 
 
 //---------------------------------------------------------------------------------------------------------
@@ -59,7 +59,7 @@ PositionedEntityROMSpec SOUND_TEST_SCREEN_STAGE_ST_UI_ENTITIES[] =
 
 FontROMSpec* const SOUND_TEST_SCREEN_STAGE_ST_FONTS[] =
 {
-	&VUENGINE_FONT,
+	&DEFAULT_FONT,
 
 	NULL
 };
