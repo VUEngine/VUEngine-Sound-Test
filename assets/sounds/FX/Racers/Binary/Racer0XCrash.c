09565b44a68f8d9fb562bf22592d6f3aded61595
Jorge Eremiev <jorgech3@gmail.com>
2019-08-29 15:07:03 -0500

    Added tap location for noise channel on a note basis.

--- assets/sounds/FX/Racers/Binary/Racer0XCrash.c
+++ assets/sounds/FX/Racers/Binary/Racer0XCrash.c
@@ -1,13 +1,13 @@
 #include <MIDI.h>
 
-#define RACER_01_SWEEP    0x6FF
-#define RACER_02_SWEEP    0x6FF - 64
-#define RACER_03_SWEEP    0x6FF + 16
-#define RACER_04_SWEEP    0x6FF + 32
-#define RACER_05_SWEEP    0x6FF - 32
-#define RACER_06_SWEEP    0x6FF + 48
-#define RACER_07_SWEEP    0x6FF
-#define RACER_08_SWEEP    0x6FF
+#define RACER_01_FREQ    0x6FF
+#define RACER_02_FREQ    0x6FF - 64
+#define RACER_03_FREQ    0x6FF + 16
+#define RACER_04_FREQ    0x6FF + 32
+#define RACER_05_FREQ    0x6FF - 32
+#define RACER_06_FREQ    0x6FF + 48
+#define RACER_07_FREQ    0x6FF
+#define RACER_08_FREQ    0x6FF
 
 #define RACER_01_DURATION 300
 #define RACER_02_DURATION 300
@@ -21,56 +21,64 @@
 
 const u16 Racer01CrashTrack1[] =
 {
-  0x000, 0x000 + RACER_01_SWEEP, ENDSOUND,
+  0x000, 0x000 + RACER_01_FREQ, ENDSOUND,
   1, RACER_01_DURATION,
-  14, 15, 
+  14, 15,
+  5, 5,
 };
 
 const u16 Racer02CrashTrack1[] =
 {
-  0x000, 0x000 + RACER_02_SWEEP, ENDSOUND,
+  0x000, 0x000 + RACER_02_FREQ, ENDSOUND,
   1, RACER_02_DURATION,
   14, 15,
+  5, 5,
 };
 
 const u16 Racer03CrashTrack1[] =
 {
-  0x000, 0x000 + RACER_03_SWEEP, ENDSOUND,
+  0x000, 0x000 + RACER_03_FREQ, ENDSOUND,
   1, RACER_03_DURATION,
   14, 15,
+  5, 5,
 };
 
 const u16 Racer04CrashTrack1[] =
 {
-  0x000, 0x000 + RACER_04_SWEEP, ENDSOUND,
+  0x000, 0x000 + RACER_04_FREQ, ENDSOUND,
   1, RACER_04_DURATION,
   14, 15,
+  5, 5,
 };
 
 const u16 Racer05CrashTrack1[] =
 {
-  0x000, 0x000 + RACER_05_SWEEP, ENDSOUND,
+  0x000, 0x000 + RACER_05_FREQ, ENDSOUND,
   1, RACER_05_DURATION,
   14, 15,
+  5, 5,
 };
 
 const u16 Racer06CrashTrack1[] =
 {
-  0x000, 0x000 + RACER_06_SWEEP, ENDSOUND,
+  0x000, 0x000 + RACER_06_FREQ, ENDSOUND,
   1, RACER_06_DURATION,
   14, 15,
+  5, 5,
 };
 
 const u16 Racer07CrashTrack1[] =
 {
-  0x000, 0x000 + RACER_07_SWEEP, ENDSOUND,
+  0x000, 0x000 + RACER_07_FREQ, ENDSOUND,
   1, RACER_07_DURATION,
-  14, 15, 
+  14, 15,
+  5, 5,
 };
 
 const u16 Racer08CrashTrack1[] =
 {
-  0x000, 0x000 + RACER_08_SWEEP, ENDSOUND,
+  0x000, 0x000 + RACER_08_FREQ, ENDSOUND,
   1, RACER_08_DURATION,
   14, 15,
+  5, 5,
 };
--- assets/sounds/FX/Racers/Binary/Racer0XRam.c
+++ assets/sounds/FX/Racers/Binary/Racer0XRam.c
@@ -1,13 +1,13 @@
 #include <MIDI.h>
 
-#define RACER_01_SWEEP    0x7F8
-#define RACER_02_SWEEP    0x7F0
-#define RACER_03_SWEEP    0x7FA
-#define RACER_04_SWEEP    0x7FC
-#define RACER_05_SWEEP    0x7F4
-#define RACER_06_SWEEP    0x7FF
-#define RACER_07_SWEEP    0x7FF
-#define RACER_08_SWEEP    0x7FF
+#define RACER_01_FREQ    0x7F8
+#define RACER_02_FREQ    0x7F0
+#define RACER_03_FREQ    0x7FA
+#define RACER_04_FREQ    0x7FC
+#define RACER_05_FREQ    0x7F4
+#define RACER_06_FREQ    0x7FF
+#define RACER_07_FREQ    0x7FF
+#define RACER_08_FREQ    0x7FF
 
 #define RACER_01_DURATION 250
 #define RACER_02_DURATION 250
@@ -21,56 +21,64 @@
 
 const u16 Racer01RamTrack1[] =
 {
-  0x000, 0x000 + RACER_01_SWEEP, ENDSOUND,
+  0x000, 0x000 + RACER_01_FREQ, ENDSOUND,
   1, RACER_01_DURATION,
   14, 15, 
+  1, 1,
 };
 
 const u16 Racer02RamTrack1[] =
 {
-  0x000, 0x000 + RACER_02_SWEEP, ENDSOUND,
+  0x000, 0x000 + RACER_02_FREQ, ENDSOUND,
   1, RACER_02_DURATION,
   14, 15,
+  1, 1,
 };
 
 const u16 Racer03RamTrack1[] =
 {
-  0x000, 0x000 + RACER_03_SWEEP, ENDSOUND,
+  0x000, 0x000 + RACER_03_FREQ, ENDSOUND,
   1, RACER_03_DURATION,
   14, 15,
+  1, 1,
 };
 
 const u16 Racer04RamTrack1[] =
 {
-  0x000, 0x000 + RACER_04_SWEEP, ENDSOUND,
+  0x000, 0x000 + RACER_04_FREQ, ENDSOUND,
   1, RACER_04_DURATION,
   14, 15,
+  1, 1,
 };
 
 const u16 Racer05RamTrack1[] =
 {
-  0x000, 0x000 + RACER_05_SWEEP, ENDSOUND,
+  0x000, 0x000 + RACER_05_FREQ, ENDSOUND,
   1, RACER_05_DURATION,
   14, 15,
+  1, 1,
 };
 
 const u16 Racer06RamTrack1[] =
 {
-  0x000, 0x000 + RACER_06_SWEEP, ENDSOUND,
+  0x000, 0x000 + RACER_06_FREQ, ENDSOUND,
   1, RACER_06_DURATION,
   14, 15,
+  1, 1,
 };
 
 const u16 Racer07RamTrack1[] =
 {
-  0x000, 0x000 + RACER_07_SWEEP, ENDSOUND,
+  0x000, 0x000 + RACER_07_FREQ, ENDSOUND,
   1, RACER_07_DURATION,
   14, 15, 
+  1, 1,
 };
 
 const u16 Racer08RamTrack1[] =
 {
-  0x000, 0x000 + RACER_08_SWEEP, ENDSOUND,
+  0x000, 0x000 + RACER_08_FREQ, ENDSOUND,
   1, RACER_08_DURATION,
   14, 15,
+  1, 1,
 };
